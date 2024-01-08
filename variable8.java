937033e43a1607ad8bdc2e519cf10a09d98cf09d
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-08 14:17:30 +0400

    Update variable8.java

--- variable8.java
+++ variable8.java
@@ -1,10 +1,10 @@
-public class variable8{
-public static void main(String[] arg){
-int a, b, c;
-a=4;
-b=5;
-c=a+b;
-System.out.println(c);
-c=6;
-}
+public class variable8 {
+  public static void main(String[] arg) {
+    int a, b, c;
+    a = 4;
+    b = 5;
+    c = a + b;
+    System.out.println(c);
+    c = 6;
+  }
 }
